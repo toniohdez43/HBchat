27066f767d15b3328d2f6e796f12690ca25a503e
Antonio <toniohdez43@users.noreply.github.com>
2016-05-09 11:32:23 -0600

    DialogCell added

--- HBchat/AppDelegate.swift
+++ HBchat/AppDelegate.swift
@@ -11,12 +11,31 @@ import xmpp_messenger_ios
 import Quickblox
 
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
     
-
+    var window: UIWindow?
+    func applicationDidEnterBackground(application: UIApplication) {
+        QBChat.instance().disconnectWithCompletionBlock { (error: NSError?) -> Void in
+            
+        }
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+        QBChat.instance().disconnectWithCompletionBlock { (error: NSError?) -> Void in
+            
+        }
+        OneChat.stop()
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        QBChat.instance().connectWithUser(QBSession.currentSession().currentUser!) { (error: NSError?) -> Void in
+            
+        }
+    }
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         QBSettings.setAccountKey("yjYy18hXswrqFSpHKbSj")
         //QBSettings.setApiEndpoint("https://api.quickblox.com", chatEndpoint: "chat.quickblox.com", forServiceZone: qbprod)
@@ -24,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         QBSettings.setAuthSecret("fNEan7pyzXwXN5V")
         QBSettings.setApplicationID(40425)
-        
+        QBSettings.setAutoReconnectEnabled(true);
         // enabling carbons for chat
         QBSettings.setCarbonsEnabled(true)
         
@@ -33,7 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Enables detailed XMPP logging in console output.
         QBSettings.enableXMPPLogging()
-        
+        QBSettings.setKeepAliveInterval(40);
         
         // Override point for customization after application launch.
         OneChat.start(true, delegate: nil) { (stream, error) -> Void in
@@ -46,30 +65,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
+    
+    
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
-    func applicationWillTerminate(application: UIApplication) {
-            OneChat.stop()
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
+    
+    
 }
-
--- HBchat/ChatsMenuController.swift
+++ HBchat/ChatsMenuController.swift
@@ -9,11 +9,79 @@
 import UIKit
 import Quickblox
 
+class DialogTableViewCellModel: NSObject {
+    
+    var detailTextLabelText: String = ""
+    var textLabelText: String = ""
+    var unreadMessagesCounterLabelText : String?
+    var unreadMessagesCounterHiden = true
+    var dialogIcon : UIImage?
+    
+    init(dialog: QBChatDialog) {
+        super.init()
+        
+        switch (dialog.type){
+        case .PublicGroup:
+            self.detailTextLabelText = "public group"
+        case .Group:
+            self.detailTextLabelText = "group"
+        case .Private:
+            self.detailTextLabelText = "private group"
+            
+            if dialog.recipientID == -1 {
+                return
+            }
+            
+            // Getting recipient from users service.
+            //if let recipient = ServicesManager.instance().usersService.usersMemoryStorage.userWithID(UInt(dialog.recipientID)) {
+            // self.textLabelText = recipient.login ?? recipient.email!
+            //}
+        }
+        
+        if self.textLabelText.isEmpty {
+            // group chat
+            
+            if let dialogName = dialog.name {
+                self.textLabelText = dialogName
+            }
+        }
+        
+        // Unread messages counter label
+        
+        if (dialog.unreadMessagesCount > 0) {
+            
+            var trimmedUnreadMessageCount : String
+            
+            if dialog.unreadMessagesCount > 99 {
+                trimmedUnreadMessageCount = "99+"
+            } else {
+                trimmedUnreadMessageCount = String(format: "%d", dialog.unreadMessagesCount)
+            }
+            
+            self.unreadMessagesCounterLabelText = trimmedUnreadMessageCount
+            self.unreadMessagesCounterHiden = false
+            
+        } else {
+            
+            self.unreadMessagesCounterLabelText = nil
+            self.unreadMessagesCounterHiden = true
+        }
+        
+        // Dialog icon
+        
+        if dialog.type == .Private {
+            self.dialogIcon = UIImage(named: "user")
+        } else {
+            self.dialogIcon = UIImage(named: "group")
+        }
+    }
+}
+
 class ChatsMenuController: UITableViewController{
     private var didEnterBackGroundDate: NSDate?
     
     override func viewWillAppear(animated: Bool) {
-       if (QBSession.currentSession().currentUser==nil)
+        if (QBSession.currentSession().currentUser==nil)
         {
             self.performSegueWithIdentifier("ToLogin", sender: self)
             print("No hay usuario")
@@ -21,10 +89,24 @@ class ChatsMenuController: UITableViewController{
         }
         else{
             print(QBSession.currentSession().currentUser!.login)
-        
+            let extendedRequest = ["sort_desc" : "_id"]
+            
+            let page = QBResponsePage(limit: 100, skip: 0)
+            
+            QBRequest.dialogsForPage(page, extendedRequest: extendedRequest, successBlock: { (response: QBResponse, dialogs: [QBChatDialog]?, dialogsUsersIDs: Set<NSNumber>?, page: QBResponsePage?) -> Void in
+                
+            }) { (response: QBResponse) -> Void in
+                
+            }
+            QBRequest.countOfDialogsWithExtendedRequest(nil, successBlock: { (response : QBResponse!, count : UInt) -> Void in
+                
+            }) { (response : QBResponse!) -> Void in
+                
+            }
+            
         }
         
     }
-
-
+    
+    
 }
